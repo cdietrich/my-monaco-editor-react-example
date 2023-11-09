cb936aa3af26d6015147bd665fe8eb88a8839208
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2023-11-09 10:27:00 +0100

    test with beta

--- src/MonacoEditorReactCompExtended.ts
+++ src/MonacoEditorReactCompExtended.ts
@@ -5,28 +5,34 @@ import { MonacoEditorProps, MonacoEditorReactComp } from "@typefox/monaco-editor
 // or should we just use composition/wrapping?
 export default class MonacoEditorReactCompExtended extends MonacoEditorReactComp<MonacoEditorPropsExtended>
 /*implements Component<MonacoEditorPropsExtended>*/ {
-    constructor(propsExt: MonacoEditorPropsExtended) {
+    constructor(private propsExt: MonacoEditorPropsExtended) {
         super(propsExt);
     }
-    // override async componentDidMount() {
-    //     console.log("didMount")
-    //     await super.componentDidMount();
-    //     const lc = this.getEditorWrapper().getLanguageClient();
-    //     console.log("lc", lc !== undefined);
-    //     await lc?.sendNotification("textDocument/didOpen", {
-    //         textDocument: {
-    //             uri: this.propsExt.otherFileUri,
-    //             version: 1,
-    //             text: this.propsExt.otherFileContent,
-    //             languageId: "hello"
-    //         }
-    //     });
-    // }
 
-    // override async componentDidUpdate(prevProps: MonacoEditorPropsExtended) {
-    //     console.log("argggggg ", prevProps.otherFileUri)
-    //     return super.componentDidUpdate(prevProps);
-    // }
+    override async componentDidMount() {
+        console.log("did mount")
+        return await super.componentDidMount();
+    }
+
+    override async startMonaco() {
+        console.log("didMount")
+        await super.startMonaco();
+        const lc = this.getEditorWrapper().getLanguageClient();
+        console.log("lc", lc !== undefined);
+        await lc?.sendNotification("textDocument/didOpen", {
+            textDocument: {
+                uri: this.propsExt.otherFileUri,
+                version: 1,
+                text: this.propsExt.otherFileContent,
+                languageId: "hello"
+            }
+        });
+    }
+
+    override async componentDidUpdate(prevProps: MonacoEditorPropsExtended) {
+        console.log("argggggg ", prevProps.otherFileUri)
+        return super.componentDidUpdate(prevProps);
+    }
 }
 
 export type MonacoEditorPropsExtended = MonacoEditorProps & {
