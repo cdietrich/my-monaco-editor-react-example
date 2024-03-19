9b98c29e6dae64cd3610dd6344322ddf8d777d84
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2024-03-19 09:54:21 +0100

    twice does still not work
    Signed-off-by: Christian Dietrich <christian.dietrich.opensource@gmail.com>

---
+++ src/App2.tsx
@@ -0,0 +1,157 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import { MonacoEditorReactComp } from '@typefox/monaco-editor-react'
+import { UserConfig, WrapperConfig } from 'monaco-editor-wrapper'
+import { useWorkerFactory } from 'monaco-editor-wrapper/workerFactory';
+import { Uri } from 'vscode';
+// import { useOpenEditorStub } from 'monaco-languageclient';
+import getConfigurationServiceOverride from '@codingame/monaco-vscode-configuration-service-override';
+// import getEditorServiceOverride from '@codingame/monaco-vscode-editor-service-override';
+// import { RegisteredFileSystemProvider, registerFileSystemOverlay, RegisteredMemoryFile } from '@codingame/monaco-vscode-files-service-override';
+import MonacoEditorReactCompExtended, { Model } from './MonacoEditorReactCompExtended';
+import { useState } from 'react';
+
+export const configureMonacoWorkers = () => {
+  // override the worker factory with your own direct definition
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useWorkerFactory({
+      ignoreMapping: true,
+      workerLoaders: {
+          editorWorkerService: () => {
+            const url = new URL('monaco-editor/esm/vs/editor/editor.worker.js', import.meta.url);
+            return new Worker(url, { type: 'module' })
+          },
+      }
+  });
+};
+configureMonacoWorkers()
+
+const getUserConfig = (workerUrl: URL, model: Model): UserConfig => {
+  const serviceConfig = {
+    workspaceConfig: {
+      workspaceProvider: {
+        trusted: true,
+        workspace: {
+          workspaceUri: Uri.file("/workspace"),
+        },
+        async open() {
+          return false;
+        },
+      },
+    },
+    userServices: {
+      ...getConfigurationServiceOverride(),
+      //   ...getEditorServiceOverride(useOpenEditorStub),
+    },
+    debugLogging: true,
+  };
+  
+  const languageId = 'hello';
+  const wrapperConfig: WrapperConfig = {
+    serviceConfig,
+    editorAppConfig: {
+      $type: 'classic' as const,
+      languageId,
+      code: model?.content ?? 'No Model specified',
+      codeUri: model?.uri ?? 'dummy.hello',
+      useDiffEditor: false,
+      editorOptions: {
+        'semanticHighlighting.enabled': true,
+      },
+      languageExtensionConfig: { id: languageId },
+      //languageDef: monarchGrammar,
+      // themeData: LangiumTheme,
+      // theme: 'langium-theme',
+      // userConfiguration: {
+      //   //   // or configure the semantic highlighting like this:
+      //   //   // `{ json: "editor.semanticHighlighting.enabled": true }`
+      //   json: '{"editor.semanticHighlighting.enabled": true}',
+      // },
+    },
+  };
+
+  const userConfig: UserConfig = {
+    wrapperConfig,
+    languageClientConfig: {
+      options: {
+        $type: 'WorkerConfig' as const,
+        url: workerUrl,
+        type: "classic" as const,
+      },
+    },
+  };
+  return userConfig;
+};
+
+
+
+// const fileSystemProvider = new RegisteredFileSystemProvider(true);
+// fileSystemProvider.registerFile(new RegisteredMemoryFile(Uri.file('/workspace/other.hello'), 'person Xxxxx person Yyyyyy'));
+// registerFileSystemOverlay(1, fileSystemProvider); 
+
+function App2() {
+
+  const [otherFiles, setOtherFiles] = useState<Model[]>([
+    {uri:  "others-demo.hello", content: "person Person1 person Person2"},
+    {uri:  "another-demo.hello", content: "person Person5"}
+   ]);
+  const [isOtherFiles1, setOtherFiles1] = useState(true);
+
+  // const workerRef = useRef<Worker>();
+  // if (workerRef.current) {
+  //   console.log("terminate");
+  //   workerRef.current.terminate();
+  // }
+  const workerURL = new URL('./hello-world-server-worker.js', window.location.origin);
+  // workerRef.current = new Worker(workerURL.href, {
+  //   type: 'classic' as const,
+  //   name: 'hello-world-language-server-worker'
+  // });
+
+  const userConfig = getUserConfig(workerURL, {
+    content: "person B Hello B! Hello Person1!",
+    uri: "demo.hello"
+  } )
+
+  const onLoad = () => {
+    console.log("mimiimi onLoad");
+  }
+
+  function handleOnClick() {
+    console.log("handleOnClick");
+    if (isOtherFiles1) {
+      setOtherFiles([
+        {uri:  "others-demo2.hello", content: "person Person3 person Person4"},
+        {uri:  "another-demo.hello", content: "person Person5"}
+       ])
+    } else {
+      setOtherFiles([
+        {uri:  "others-demo.hello", content: "person Person1 person Person2"},
+        {uri:  "another-demo.hello", content: "person Person5"}
+       ])
+    }
+    setOtherFiles1(!isOtherFiles1);
+  }
+  // 
+  return (
+    <div className="lulu">
+      <button onClick={handleOnClick}>Click Me!</button>
+      <MonacoEditorReactCompExtended
+    // key={new Date().toISOString() /* without this the updated monaco seems to not usable at all */}  
+    userConfig={userConfig}
+    onLoad={onLoad}
+    otherFiles={otherFiles}
+    style={{
+      paddingTop: '5px',
+      height: '40vh',
+      width: '100%',
+    }}
+    
+        />
+    </div>
+  )
+}
+
+export default App2
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import App2 from './App2.tsx'
 //import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />,
+    <div>
+ <App /> <App2 />
+
+    </div>
+   ,
 )
