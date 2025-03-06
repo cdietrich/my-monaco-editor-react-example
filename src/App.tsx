d2de227a136fcb392427a68940471a5bfef4d5cc
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2025-03-06 12:38:03 +0100

    Update versions (#3)
    * experiments with other files
    
    * consistent versions
    
    Signed-off-by: Christian Dietrich <christian.dietrich.opensource@gmail.com>
    
    * Implement Opening and Closing other files (#2)
    
    Implement Opening and Closing other files
    
    * switch to didClose
    
    * another bump
    
    * added hint on didClose upstream
    
    * another bump round
    
    * update to 2.4.0 release
    
    * experiment with createModelReference
    
    * added sample for multiple other files
    
    * added update support to avoid open close races
    
    Signed-off-by: Christian Dietrich <christian.dietrich.opensource@gmail.com>
    
    * another bump
    
    * another bump round
    
    * fix resolution versions
    
    * fix deprecation
    
    * harmonize ds
    
    * dont hardcode language id
    
    * update to langium 3
    
    * bump
    
    * more cleanup
    
    * even more cleanup
    
    * even more cleanup
    
    * more simpl
    
    * update transitive deps
    
    * further cleanup
    
    * just worker url
    
    * added 2way binding sample
    
    * make it a bit bigger and smaller
    
    * more clean
    
    * newer worker
    
    * More stuff<
    
    * update to 6.0.0
    
    * use the release
    
    * bump some versions
    
    * update to 6.1.0
    
    * wip: another update round
    
    * another bump attempt
    
    * another preview bump
    
    * another bump
    
    * final bump to 6.5.0
    
    ---------
    
    Signed-off-by: Christian Dietrich <christian.dietrich.opensource@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,174 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-import { MonacoEditorReactComp } from '@typefox/monaco-editor-react'
-import { UserConfig, WrapperConfig } from 'monaco-editor-wrapper'
-import { buildWorkerDefinition } from 'monaco-editor-workers';
-// import { Uri } from 'vscode';
-// import { useOpenEditorStub } from 'monaco-languageclient';
-// import getConfigurationServiceOverride from '@codingame/monaco-vscode-configuration-service-override';
-// import getEditorServiceOverride from '@codingame/monaco-vscode-editor-service-override';
+import getConfigurationServiceOverride from "@codingame/monaco-vscode-configuration-service-override"
+import getKeybindingsServiceOverride from "@codingame/monaco-vscode-keybindings-service-override"
+import { useWorkerFactory, type WorkerLoader } from 'monaco-languageclient/workerFactory';
+import type * as monaco from "@codingame/monaco-vscode-editor-api"
+import { WrapperConfig } from 'monaco-editor-wrapper'
+import { Uri } from 'vscode';
+import MonacoEditorReactCompExtended, { Model } from './MonacoEditorReactCompExtended';
+import { useState } from 'react';
+import type { Logger } from "monaco-languageclient/tools"
+const languageId = 'hello';
 
-buildWorkerDefinition('../../../../node_modules/monaco-editor-workers/dist/workers', import.meta.url, false);
+const defineDefaultWorkerLoaders: () => Record<string, WorkerLoader> = () => {
+  return {
+    TextEditorWorker: () => new Worker(
+      new URL('@codingame/monaco-vscode-editor-api/esm/vs/editor/editor.worker.js', import.meta.url),
+      { type: 'module' }
+    ),
+    // these are other possible workers not configured by default
+    TextMateWorker: undefined,
+    OutputLinkDetectionWorker: undefined,
+    LanguageDetectionWorker: undefined,
+    NotebookEditorWorker: undefined,
+    LocalFileSearchWorker: undefined,
+  }
+}
 
-const getUserConfig = (lsWorker: Worker, model: {code?:string, uri?: string}) => {
-  const serviceConfig = {
-    // userServices: {
-    //   ...getConfigurationServiceOverride(Uri.file('/workspace')),
-    //   ...getEditorServiceOverride(useOpenEditorStub),
-    // },
-    debugLogging: true,
-  };
-  const languageId = 'hello';
-  const wrapperConfig: WrapperConfig = {
-    serviceConfig,
+const getUserConfig = (
+  workerUrl: string,
+  fileExt: string,
+  htmlElement: HTMLElement,
+  model: Model,
+  monarch?: monaco.languages.IMonarchLanguage,)
+  
+  
+  
+   : WrapperConfig => {
+  //const fileExt = model.uri.split('.').pop() ?? 'hello';
+  const loadLangiumWorker = () => {
+    console.log(`Langium worker URL: ${workerUrl}`)
+    const workerURL = new URL('./hello-world-server-worker.js', window.location.origin)
+    return new Worker(workerURL, {
+      type: "module",
+      name: "Lotse Language Server Worker",
+    })
+  }
+  const langiumWorker = loadLangiumWorker()
+  return {
+    $type: "classic",
+    htmlContainer: htmlElement,
+    vscodeApiConfig: {
+      serviceOverrides: {
+        ...getConfigurationServiceOverride(),
+        //...getEditorServiceOverride(useOpenEditorStub),
+        ...getKeybindingsServiceOverride(),
+      },
+    },
     editorAppConfig: {
-      $type: 'classic' as const,
-      languageId,
-      code: model?.code ?? 'No Model specified',
-      codeUri: model?.uri ?? 'dummy.hello',
+      codeResources: {
+        modified: {
+          text: model.content,
+          fileExt,
+          enforceLanguageId: model.languageId,
+        },
+      },
       useDiffEditor: false,
+      languageDef: {
+        monarchLanguage: monarch,
+        languageExtensionConfig: {
+          id: model.languageId,
+          extensions: [`.${fileExt}`],
+        },
+      },
       editorOptions: {
-        'semanticHighlighting.enabled': true,
+        "semanticHighlighting.enabled": true,
+        theme: "vs-dark",
       },
-      languageExtensionConfig: { id: languageId },
-      //languageDef: monarchGrammar,
-      // themeData: LangiumTheme,
-      // theme: 'langium-theme',
-      // userConfiguration: {
-      //   //   // or configure the semantic highlighting like this:
-      //   //   // `{ json: "editor.semanticHighlighting.enabled": true }`
-      //   json: '{"editor.semanticHighlighting.enabled": true}',
-      // },
+      monacoWorkerFactory: (logger?: Logger) => {
+        useWorkerFactory({
+          workerLoaders: defineDefaultWorkerLoaders(),
+          logger,
+        })
+      },
     },
-  };
+    languageClientConfigs: {
+      configs: {
+        lotse: {
+          connection: {
+            options: {
+              $type: "WorkerDirect",
+              worker: langiumWorker,
+            },
+          },
+          clientOptions: {
+            documentSelector: [languageId],
+          },
+        },
+      }
+    },
+  }
 
-  const userConfig: UserConfig = {
-    wrapperConfig,
-    languageClientConfig: {
-      options: {
-        $type: 'WorkerDirect' as const,
-        worker: lsWorker,
-      },
-    },
-  };
-  return userConfig;
+
+
 };
-
-const workerURL = new URL('./hello-world-server-worker.js', window.location.origin);
-const lsWorker = new Worker(workerURL.href, {
-  type: 'classic' as const,
-  name: 'hello-world-language-server-worker'
-});
 
 function App() {
 
-  const userConfig = getUserConfig(lsWorker, {
-    code: "person A Hello A!",
-    uri: "demo.hello"
+  const [otherFiles, setOtherFiles] = useState<Model[]>([
+    {uri:  "others-demo.hello", content: "person Person1 person Person2", languageId: 'hello'},
+    {uri:  "another-demo.hello", content: "person Person5", languageId: 'hello'}
+   ]);
+  const [isOtherFiles1, setOtherFiles1] = useState(true);
+
+  const initialModel = "person A Hello A! Hello Person1!";
+  const [modelContent, setModelContent] = useState(initialModel)
+  const [modelContent2, setModelContent2] = useState(initialModel)
+
+  //const workerURL = new URL('./hello-world-server-worker.js', window.location.origin);
+
+  const wrapperConfig = getUserConfig(
+    
+    './hello-world-server-worker.js', "hello", document.getElementById('root')!, {
+    content: modelContent,
+    uri: "demo.hello",
+    languageId: "hello",
   } )
 
+  const onLoad = () => {
+    console.log("mimimi onLoad");
+  }
+
+  function handleOnClick() {
+    if (isOtherFiles1) {
+      setOtherFiles([
+        {uri:  "others-demo2.hello", content: "person Person3 person Person4",languageId: "hello"},
+        {uri:  "another-demo.hello", content: "person Person5",languageId: "hello"}
+       ])
+    } else {
+      setOtherFiles([
+        {uri:  "others-demo.hello", content: "person Person1 person Person2",languageId: "hello"},
+        {uri:  "another-demo.hello", content: "person Person5",languageId: "hello"}
+       ])
+    }
+    setOtherFiles1(!isOtherFiles1);
+  }
+  // 
   return (
-    <div>
-      <MonacoEditorReactComp
-      key={new Date().toISOString()}
-    userConfig={userConfig}
+    <div className="lulu">
+      <button onClick={handleOnClick}>Click Me!</button>
+      <MonacoEditorReactCompExtended
+      otherFiles={otherFiles}
+      wrapperConfig={wrapperConfig}
+    onLoad={onLoad}
+    // onTextChanged={(text) => { setModelContent2(text.main) }}
     style={{
       paddingTop: '5px',
       height: '40vh',
-      width: '100%',
+      width: '90%',
     }}
+    
         />
+
+   
+        {/* <textarea
+        value={modelContent2}
+        style={{width: '90%', height: '40vh'}}
+        onChange={(e) => {
+          const newValue = e.target.value;
+          if (newValue !== modelContent) { // Only update if the value has changed
+            setModelContent(newValue);
+          }
+        }}/>  */}
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 //import './index.css'
 
+//import { useWorkerFactory } from 'monaco-editor-wrapper/workerFactory';
+
+// const configureMonacoWorkers = () => {
+//     // eslint-disable-next-line react-hooks/rules-of-hooks
+//     useWorkerFactory({
+//         ignoreMapping: true,
+//         workerLoaders: {
+//             editorWorkerService: () => new Worker(new URL('monaco-editor/esm/vs/editor/editor.worker.js', import.meta.url), { type: 'module' }),
+//         }
+//     });
+//   };
+// configureMonacoWorkers();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />,
 )
